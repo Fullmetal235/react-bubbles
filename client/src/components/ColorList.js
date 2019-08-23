6d015b0d7a4682dd12bb18afa2bd0485c94d08fa
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-08-23 12:42:31 -0500

    updated ColorList with put and delete

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +22,24 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth.put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(response => {
+      const updatedArray = colors.filter(item => item.id !== colorToEdit.id)
+      updatedArray.push(response.data)
+      updateColors(updatedArray)
+      console.log('updated array', updatedArray)})
+    .catch(error => console.log(error.response))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then(response => {
+      const newColorArray = colors.filter(item => item.id !== color.id)
+      updateColors(newColorArray)
+      console.log('delete response', response.data)
+      console.log('new color array', newColorArray)})
+    .catch(error => console.log(error.response))
   };
 
   return (
